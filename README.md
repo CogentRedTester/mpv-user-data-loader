35db275a64e0dd4f8e93cff5d9d6c7489f947421
CogentRedTester <cogent.redtester@outlook.com>
2023-12-20 23:28:16 +1030

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,149 @@
 # mpv-user-data-loader
-A script that allows the mpv `user-data` property to be set at launch and runtime using config files.
+A script that allows the mpv `user-data` property to be more easily
+configured using config files.
+
+The script does this in two ways:
+* `user-data` fields can be set on startup using config files.
+* `user-data` fields can be set during runtime using `script-opts`.
+
+## Changing Properties at Startup
+There are a few different ways of setting
+`user-data` fields at startup.
+
+### Config File
+`~~/script-opts/user-data.conf`
+
+This is a config file that uses the standard
+[script config syntax](https://mpv.io/manual/master/#lua-scripting-on-update]]%29)
+of `<key>=<value>`.
+
+`key` is the path to the `user-data` field that will be modified, e.g.
+`script_name/visibility`.
+`value` is the JSON formatted value that will be set to that field.
+The `/` and `=` characters are not supported in any of the field names for this
+config file, even though the `user-data` property does support those characters.
+
+Example:
+
+```properties
+# comment
+script_name/visibility=true
+script2/number-of-items=21
+script2/text/header="header text"
+```
+
+`user-data` output:
+```json
+{
+    "script_name": {
+        "visibility":true
+    },
+    "script2": {
+        "number-of-items": 21,
+        "text": {
+            "header": "header text"
+        }
+    }
+}
+```
+
+### JSON Config File
+`~~/script-opts/user-data.json`
+
+This config file uses JSON syntax, which should make it
+easier to define highly nested `user-data` values.
+
+The file represents the full `user-data` property,
+so to replicate the previous example
+the json file could simply contain:
+
+```json
+{
+    "script_name": { "visibility":true },
+    "script2": {
+        "number-of-items": 21,
+        "text": { "header": "header text" }
+    }
+}
+```
+
+Note that all of the top-level fields (e.g. `script_name` and `script2`)
+directly overwrite the existing value in `user-data`.
+This means that if `script_name/other_field` exists in `user-data`,
+it will be removed after `"script_name": { "visibility":true }` is applied.
+This will likely change in the future.
+
+The JSON file can be used in conjunction with `user-data.conf`.
+The JSON file will be applied first, so will not overwrite
+the values set in `user-data.conf`.
+
+### `script-opts`
+The [`script-opts`](https://mpv.io/manual/master/#options-script-opts)
+property can also be set at startup using `mpv.conf` or with
+commandline arguments. See
+[changing properties at runtime](#changing-properties-at-runtime) for
+the correct syntax.
+
+Values set with script-opts always overwrite the config files.
+
+## Changing Properties at Runtime
+`user-data` fields can be modified at runtime using
+[`script-opts`](https://mpv.io/manual/master/#options-script-opts).
+This allows thew fields to be set from the commandline, from
+`mpv.conf`, and from profiles, including
+[conditional auto profiles](https://mpv.io/manual/master/#conditional-auto-profiles).
+
+The script-opt is in the form `user-data/path/to/field=<JSON value>`.
+It is nearly identical to the `user-data.conf` syntax, except that
+the keys must be prefixed with `user-data/`.
+
+If a `script-opt` is removed, then the associated `user-data` field is
+reset to the value it had when the first script-opt was applied.
+This nearly equivalent to the behaviour of
+[`profile-restore=copy`](https://mpv.io/manual/master/#runtime-profiles).
+An option to use `profile-restore=copy-equal` behaviour may be added
+in the future.
+
+### Examples
+
+#### `mpv.conf`
+
+```properties
+script-opts-append=user-data/script_name/visibility=true
+script-opts-append=user-data/script2/number-of-items=21
+script-opts-append=user-data/script2/text/header="header text"
+```
+
+Applying the script-opts with a profile:
+```properties
+[profile1]
+script-opts-append=user-data/script_name/visibility=true
+script-opts-append=user-data/script2/number-of-items=21
+script-opts-append=user-data/script2/text/header="header text"
+```
+
+#### Conditional Auto Profiles
+Sets a `monitor-name` field depending on
+which monitor the mpv window is currently displayed on.
+
+```properties
+[DELL]
+profile-cond=display_names[1] == 'DP-1'
+profile-restore=copy
+script-opts-append=user-data/monitor-name="DELL Monitor"
+
+[OtherBrand]
+profile-cond=display_names[1] ~= 'DP-1'
+profile-restore=copy
+script-opts-append=user-data/monitor-name="Other Monitor"
+```
+
+#### Commandline
+
+```bash
+mpv --script-opts-append='user-data/script_name/visibility=true' \
+    --script-opts-append='user-data/script2/number-of-items=21' \
+    --script-opts-append='user-data/script2/text/header="header text"'
+```
+
+Make sure that the `"` characters around strings are not stripped by the shell.
---
+++ user-data-loader.lua
@@ -0,0 +1,162 @@
+--[[
+    A simple script that attempts to make it easier to configure
+    the `user-data` property using config files.
+
+    The script has two features:
+        - user-data properties can be set on startup using config files
+        - user-data can be set during runtime using script-opts
+    See the README for documentation.
+
+    This file is under the MIT license, see Github for the license file.
+
+    Project available at: https://github.com/CogentRedTester/mpv-user-data-loader
+]]
+
+local mp = require 'mp'
+local msg = require 'mp.msg'
+local utils = require 'mp.utils'
+
+local overrides = {}
+local prev_values = {}
+
+local config_path = mp.command_native({'expand-path', '~~/script-opts/user-data.conf'})
+local json_config_file = mp.command_native({'expand-path', '~~/script-opts/user-data.json'})
+
+-- tests if two complex values are equal
+local function is_equal(a,b)
+    return utils.to_string(a) == utils.to_string(b)
+end
+
+-- Sets a field of the user-data property.
+-- Appends `user-data/` to `key` and parses `value` as a json string.
+local function set_value(key, value)
+    local ud_key = 'user-data/'..key
+    local ud_value, err = utils.parse_json(value)
+
+    local prev_val = mp.get_property_native(ud_key)
+
+    if (err) then
+        msg.error(err)
+        msg.warn('failed to parse user-data JSON string:', value)
+        return false, ud_key, ud_value, prev_val
+    end
+
+    if not is_equal(prev_val, ud_key) then
+        msg.verbose('setting', ud_key, utils.to_string(ud_value))
+        mp.set_property_native(ud_key, ud_value)
+    end
+
+    return true, ud_key, ud_value, prev_val
+end
+
+-- Sets a field of the user-data property.
+-- Appends `user-data/` to `key` and passes value through natively.
+local function set_value_native(key, ud_value)
+    local ud_key = 'user-data/'..key
+    local prev_val = mp.get_property_native(ud_key)
+
+    if not is_equal(prev_val, ud_key) then
+        msg.verbose('setting', ud_key, utils.to_string(ud_value))
+        mp.set_property_native(ud_key, ud_value)
+    end
+
+    return true, ud_key, ud_value, prev_val
+end
+
+-- loading the values from user-data.conf
+-- uses normal script-opts syntax
+local function setup_config()
+    local file = io.open(config_path, 'r')
+    if not file then
+        msg.debug('could not read', config_path)
+        return
+    end
+
+    msg.debug('reading values from', config_path)
+
+    for line in file:lines() do
+        if line ~= '' and string.sub(line, 1, 1) ~= '#' then
+            local key, value = string.match(line, '^([^=]+)=(.+)')
+            if not key or not value then
+                msg.error('invalid line in config file: "'..line..'"')
+            else
+                set_value(key, value)
+            end
+        end
+    end
+
+    file:close()
+end
+
+-- loading values from user-data.json
+-- uses JSON syntax
+local function setup_json()
+    local file = io.open(json_config_file, 'r')
+    if not file then
+        msg.debug('could not read', json_config_file)
+        return
+    end
+
+    msg.debug('reading values from', json_config_file)
+
+    local json, err = utils.parse_json(file:read("*a"))
+    if err then
+        msg.error(err, 'failed to parse JSON file', json_config_file)
+        msg.warn('check the syntax!')
+    elseif type(json) ~= 'table' then
+        msg.warn(json_config_file)
+        msg.error('json file must be a an object')
+    else
+        for key, value in pairs(json) do
+            set_value_native(key, value)
+        end
+    end
+end
+
+-- detects changes to the script-opts property that requires `user-data` updates
+local function main(_, opts)
+    local current_overrides = {}
+
+    -- Finds any user-data value overrides in `script-opts`.
+    for key, value in pairs(opts) do
+        if string.find(key, '^user%-data/.') then
+            local succeeds, ud_key, ud_value, prev_value = set_value(string.match(key, '^user%-data/(.+)') or '', value)
+
+            if succeeds then
+                current_overrides[ud_key] = true
+
+                if not overrides[ud_key] then
+                    overrides[ud_key] = true
+                    prev_values[ud_key] = prev_value
+                end
+            end
+        end
+    end
+
+    -- If any values have been removed from script-opts we want to reset them to the original values.
+    -- This is to add synergy with conditional auto profiles.
+    for key in pairs(overrides) do
+        if not current_overrides[key] then
+            local prev_value = prev_values[key]
+            if prev_value then
+                msg.verbose('setting', key, utils.to_string(prev_value))
+                mp.set_property_native(key, prev_value)
+            else
+                msg.verbose('deleting', key)
+                mp.del_property(key)
+            end
+            overrides[key] = nil
+            prev_values[key] = nil
+        end
+    end
+end
+
+-- Loading initial user-data values from config files
+local function setup()
+    msg.debug('reading initial values from config files')
+    setup_json()
+    setup_config()
+end
+
+setup()
+mp.observe_property('script-opts', 'native', main)
